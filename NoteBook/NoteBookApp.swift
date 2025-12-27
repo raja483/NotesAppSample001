fc59cd0c5c789bba60b8294b32277b540ea1e6c4
Rajasekhar <j.raja483@gmail.com>
2025-12-27 14:14:01 +0530

    Initial Commit

---
+++ NoteBook/NoteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteBookApp.swift
+//  NoteBook
+//
+//  Created by Rajasekhar on 27/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
